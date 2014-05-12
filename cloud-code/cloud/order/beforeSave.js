6119feba4b341d72b66d5e5e709c41aad24c7d92
Luís Eduardo Brito <luiseduardo14@gmail.com>
2014-05-12 11:18:46 -0300

    rascunho de metodos de place order

--- cloud-code/cloud/order/beforeSave.js
+++ cloud-code/cloud/order/beforeSave.js
@@ -10,10 +10,22 @@ var BeforeSave = function(request, response) {
 
 	else if (!request.object.get("total") || request.object.get("total") <= 0) {
 		response.error("You have to specify a valid total value");
-	} 
+	}
+
+	else if(!request.object.get("items")) {
+		response.error("You have to specify a valid items list");
+	}
 
 	else {
+
+		// Create new sms code
 		request.object.set('smsCode', random(1000, 10000));
+
+		// TODO: generate tickets
+
+		// TODO: place order in payment gateway
+
+		// Return result
 		response.success();
 	}
 }
